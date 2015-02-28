7b5ea46cb89b52fdf844512e39bcfc299fda98de
Maggie Yu <shuangy@andrew.cmu.edu>
2015-02-28 12:52:57 -0500

    scaffolded contacts

---
+++ Amber/db/migrate/20150228175003_create_contacts.rb
@@ -0,0 +1,13 @@
+class CreateContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.string :patient_id
+
+      t.timestamps
+    end
+  end
+end
