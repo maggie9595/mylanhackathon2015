9e6e54b7aa4006d0a63d36b7b8472ec909e2d0ba
Maggie Yu <shuangy@andrew.cmu.edu>
2015-02-28 14:28:45 -0500

    patient relationships

--- Amber/app/models/patient.rb
+++ Amber/app/models/patient.rb
@@ -1,2 +1,7 @@
 class Patient < ActiveRecord::Base
+  # Relationships
+  # -----------------------------
+  has_many :contacts
+  has_many :prescriptions
+  has_many :medications, through: :prescriptions
 end
