210dd8c0f3eee5c8b674c925a711b0308290b74b
Ally Sorge <asorge@cmu.edu>
2015-02-28 13:56:29 -0500

    changed routes

--- amber/config/routes.rb
+++ amber/config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+ # set the root
+  root :to => "patients#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
