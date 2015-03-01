c49ce69ef186fbffaeb9179056f0ca3d0c62467a
Maggie Yu <shuangy@andrew.cmu.edu>
2015-02-28 22:16:28 -0500

    front page pics show up now

--- Amber/app/assets/javascripts/script.js
+++ Amber/app/assets/javascripts/script.js
@@ -19,17 +19,4 @@ $(document).ready(function(){
     $(window).resize(function() {
         resizeContent();
     });
-
-    $('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash;
-	    var $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
 });
